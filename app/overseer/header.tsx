f303e389015408f2acb5e9d5995d1227a172f488
1Omon <1serwise@gmail.com>
2024-10-31 05:32:01 +0000

    added overseer's section

---
+++ app/overseer/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Overseer Header</div>
+  )
+}
+
+export default Header
---
+++ app/overseer/wrights/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>wrights page</div>
+  )
+}
+
+export default page
