911df720171352e772cab0a532c53ad661a501c8
Jessica <jessicaennor@gmail.com>
2024-12-04 16:25:58 +0100

    feat: modified library file

--- app/school/library/page.tsx
+++ app/school/library/page.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>library page</div>
+    <div className='bg-[#e2ecfd]'>
+      Discover
+<div>
+   <input type="search" name="" id="" placeholder='Search by name,author, etc'
+     className='text-sm p-2 rounded-md ' />
+      <button className='bg-[#9D2292] text-white py-1 px-2 text-sm'>Search</button>
+</div>
+ <h1 className='text-xl font-medium'>Recommendation</h1>
+ <p>
+  From Us
+  </p>    
+
+    </div>
   )
 }
 
