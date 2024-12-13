064fadd6977caaf2435d91987500e2c4bf5a7658
Jessica <jessicaennor@gmail.com>
2024-12-13 17:26:35 +0100

    feat(profile-card): modified profile-card file

--- components/profile-card.tsx
+++ components/profile-card.tsx
@@ -1,20 +1,37 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import { FaLinkedin } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+import { BsTwitterX } from "react-icons/bs";
 
 interface IUserProfile {
-  name: string
-  image: string
-  
+  name: string;
+  image: string;
 }
 
 const ProfileCard = () => {
   return (
-    <div className='w-full h-[225px] bg-school-neutral flex flex-col flex-center'>
-        <Image src={'/images/jess.jpeg'} alt='' width={100} height={100} className='rounded-full object-cover' />
-        <h3 className="text-xl font-medium">Jessica Baze</h3>
-        <p>Aspiring Software Developer</p>
-    </div>
-  )
-}
+    <>
+      <div className="bg-school-neutral w-full h-[225px]">
+        <div className="flex flex-col flex-center">
+          <Image
+            src={"/images/jess.jpeg"}
+            alt=""
+            width={100}
+            height={100}
+            className="rounded-full object-cover"
+          />
+          <h3 className="text-xl font-medium">Jessica Baze</h3>
+          <p>Aspiring Software Developer</p>
+        </div>
+        <div className="flex justify-center space-x-6 m-2">
+          <FaLinkedin />
+          <MdOutlineEmail />
+          <BsTwitterX />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default ProfileCard
+export default ProfileCard;
