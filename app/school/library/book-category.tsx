51cff8d8fbfdf4f9fa4dd028292e15dc112afd72
Jessica <jessicaennor@gmail.com>
2024-12-16 14:17:11 +0100

    feat:modified books.json and book category files

--- app/school/library/book-category.tsx
+++ app/school/library/book-category.tsx
@@ -1,21 +1,31 @@
 import Image from 'next/image'
 import React from 'react'
 
-const BookCategory = () => {
+type BookType = {
+  id: string
+  title: string
+  subtitle: string
+  image: string
+  author: string
+  url: string
+  }
+const BookCategory = ({book}:{book:BookType}) => {
+  
+  console.log(book);
   return (
     <div className="bg-white p-4 rounded-md shadow-md w-full flex">
       <div className="w-1/2 h-full">
         <Image
           src="/icons/bgIcon.jpg"
           alt="Image 1"
-          width={32}
+          width={82}
           height={16}
           className="w-full"
         />
       </div>
       <div className="w-1/2">
-        <h2 className="text-lg font-semibold">books</h2>
-        <p className="text-gray-600">book description</p>
+        <h2 className="font-semibold mb-2">{book.title}</h2>
+        <p className="text-gray-600">{book.subtitle}</p>
       </div>
     </div>
   )
