b5dd27e7f757a843bd364887b626b723b6fddb7d
1Omon <1serwise@gmail.com>
2025-01-01 21:08:47 +0000

    working on home page

--- app/school/library/history.tsx
+++ app/school/library/history.tsx
@@ -1,27 +1,25 @@
 import Image from "next/image";
-const BookHistory = () => {
 
+const BookHistory = () => {
   return (
     <>
-    <div className="bg-white rounded-md shadow-md w-full flex mt-4">
-        
-      <div className="h-full mr-4">
-        <Image
-          src="/icons/bgIcon.jpg"
-          alt="Image 1"
-          width={82}
-          height={16}
-          className=""
-        />
+      <div className="bg-white rounded-md shadow-md w-full flex mt-4">
+        <div className="h-full mr-4">
+          <Image
+            src="/icons/bgIcon.jpg"
+            alt="Image 1"
+            width={82}
+            height={16}
+            className=""
+          />
+        </div>
+        <div className="w-1/2">
+          <h2 className="font-semibold mb-2">books title</h2>
+          <p className="text-gray-600">description</p>
+        </div>
       </div>
-      <div className="w-1/2">
-        <h2 className="font-semibold mb-2">books title</h2>
-        <p className="text-gray-600">description</p>
-      </div>
-    </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default BookHistory
+export default BookHistory;
