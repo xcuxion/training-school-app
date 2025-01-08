ea54f88a601b4ff0f2a39ebd86ecd9b337e41dcf
Jessica <jessicaennor@gmail.com>
2025-01-08 23:02:57 +0100

    feat:Set up jest for testing

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+module.exports = {
+    testEnvironment: 'jest-environment-jsdom',
+    moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+};
