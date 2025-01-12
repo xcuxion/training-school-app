dea181879695fdfa4d9d8e0b278da419f8e04a47
1Omon <1serwise@gmail.com>
2025-01-12 23:07:46 +0000

    made portfolio and form pages responsive

--- components/portfolio/TeamMemberCard.tsx
+++ components/portfolio/TeamMemberCard.tsx
@@ -10,7 +10,7 @@ interface TeamMemberCardProps {
 const TeamMemberCard = ({ name, role, image }: TeamMemberCardProps) => {
   return (
       <div className="w-full">
-        <Image src={image} alt="" width={450} height={300} className="w-[400px] h-[300px] object-cover object-top rounded-md" />
+        <Image src={image} alt="" width={450} height={300} className="md:w-[400px] md:h-[300px] object-cover object-top rounded-md" />
         <h1 className="text-xl font-semibold">{name}</h1>
         <p className="text-base">{role}</p>
       </div>
