aa59b10e3a41c35853f40b1790062d8beaf9989f
1Omon <1serwise@gmail.com>
2025-01-15 09:37:58 +0000

    deleted previous migrations

--- prisma/migrations/20250112212558_optimized/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `dob` on the `Applicant` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "Applicant" DROP COLUMN "dob",
-ADD COLUMN     "dob" DATE NOT NULL;
---
+++ prisma/migrations/20250115093408_initial/migration.sql
@@ -0,0 +1,229 @@
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('male', 'female');
+
+-- CreateEnum
+CREATE TYPE "School" AS ENUM ('knust', 'ug', 'ashesi', 'none');
+
+-- CreateEnum
+CREATE TYPE "Scholarship" AS ENUM ('yes', 'no');
+
+-- CreateEnum
+CREATE TYPE "ResourceType" AS ENUM ('video', 'audiobook', 'ebook');
+
+-- CreateEnum
+CREATE TYPE "Category" AS ENUM ('marketing', 'technology', 'entrepreneurship', 'strategy');
+
+-- CreateEnum
+CREATE TYPE "TaskStatus" AS ENUM ('todo', 'in_progress', 'completed');
+
+-- CreateEnum
+CREATE TYPE "TaskPriority" AS ENUM ('low', 'medium', 'high');
+
+-- CreateEnum
+CREATE TYPE "AdmissionStatus" AS ENUM ('pending', 'admitted', 'rejected');
+
+-- CreateTable
+CREATE TABLE "Enquiry" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "email" VARCHAR(80) NOT NULL,
+    "question" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Enquiry_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Applicant" (
+    "profileId" TEXT,
+    "id" TEXT NOT NULL,
+    "fname" VARCHAR(255) NOT NULL,
+    "lname" VARCHAR(255) NOT NULL,
+    "oname" VARCHAR(255),
+    "dob" DATE NOT NULL,
+    "gender" "Gender" NOT NULL,
+    "nationality" VARCHAR(255) NOT NULL,
+    "school" "School" NOT NULL,
+    "contact" VARCHAR(10) NOT NULL,
+    "programme" VARCHAR(255) NOT NULL,
+    "year" VARCHAR(4) NOT NULL,
+    "reason" VARCHAR(255) NOT NULL,
+    "balance" VARCHAR(255) NOT NULL,
+    "scholarship" "Scholarship" NOT NULL,
+    "status" "AdmissionStatus" NOT NULL DEFAULT 'pending',
+    "statement" VARCHAR(255),
+
+    CONSTRAINT "Applicant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Student" (
+    "applicantId" TEXT,
+    "reference" TEXT NOT NULL,
+    "rewardPoints" INTEGER NOT NULL DEFAULT 0,
+    "feeId" TEXT NOT NULL,
+    "batch" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Student_pkey" PRIMARY KEY ("reference")
+);
+
+-- CreateTable
+CREATE TABLE "Fee" (
+    "id" TEXT NOT NULL,
+    "scholarship" BOOLEAN NOT NULL DEFAULT false,
+    "balance" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "Fee_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" VARCHAR(80) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Facilitator" (
+    "profileId" TEXT,
+    "id" TEXT NOT NULL,
+    "bio" VARCHAR(255) NOT NULL,
+    "moduleId" TEXT NOT NULL,
+
+    CONSTRAINT "Facilitator_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Receipt" (
+    "id" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "paymentMode" VARCHAR(255) NOT NULL,
+    "feeId" TEXT,
+
+    CONSTRAINT "Receipt_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Case" (
+    "id" TEXT NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "responsibilities" TEXT[],
+    "moduleId" TEXT,
+
+    CONSTRAINT "Case_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Resource" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "type" "ResourceType" NOT NULL,
+    "category" "Category" NOT NULL,
+    "url" VARCHAR(255) NOT NULL,
+    "moduleId" TEXT,
+    "caseId" TEXT,
+
+    CONSTRAINT "Resource_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Module" (
+    "id" TEXT NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Module_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "summary" VARCHAR(255) NOT NULL,
+    "dueDate" TIMESTAMP(3) NOT NULL,
+    "status" "TaskStatus" NOT NULL DEFAULT 'todo',
+    "priority" "TaskPriority" NOT NULL DEFAULT 'medium',
+    "comments" TEXT[],
+    "moduleId" TEXT,
+    "sprintId" TEXT,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Sprint" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "Sprint_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_StudentTasks" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_StudentTasks_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Applicant_profileId_key" ON "Applicant"("profileId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Student_applicantId_key" ON "Student"("applicantId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Student_feeId_key" ON "Student"("feeId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_email_key" ON "Profile"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Facilitator_profileId_key" ON "Facilitator"("profileId");
+
+-- CreateIndex
+CREATE INDEX "_StudentTasks_B_index" ON "_StudentTasks"("B");
+
+-- AddForeignKey
+ALTER TABLE "Applicant" ADD CONSTRAINT "Applicant_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Student" ADD CONSTRAINT "Student_applicantId_fkey" FOREIGN KEY ("applicantId") REFERENCES "Applicant"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Student" ADD CONSTRAINT "Student_feeId_fkey" FOREIGN KEY ("feeId") REFERENCES "Fee"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Facilitator" ADD CONSTRAINT "Facilitator_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Facilitator" ADD CONSTRAINT "Facilitator_moduleId_fkey" FOREIGN KEY ("moduleId") REFERENCES "Module"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Receipt" ADD CONSTRAINT "Receipt_feeId_fkey" FOREIGN KEY ("feeId") REFERENCES "Fee"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Case" ADD CONSTRAINT "Case_moduleId_fkey" FOREIGN KEY ("moduleId") REFERENCES "Module"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Resource" ADD CONSTRAINT "Resource_moduleId_fkey" FOREIGN KEY ("moduleId") REFERENCES "Module"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Resource" ADD CONSTRAINT "Resource_caseId_fkey" FOREIGN KEY ("caseId") REFERENCES "Case"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_moduleId_fkey" FOREIGN KEY ("moduleId") REFERENCES "Module"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_sprintId_fkey" FOREIGN KEY ("sprintId") REFERENCES "Sprint"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_StudentTasks" ADD CONSTRAINT "_StudentTasks_A_fkey" FOREIGN KEY ("A") REFERENCES "Student"("reference") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_StudentTasks" ADD CONSTRAINT "_StudentTasks_B_fkey" FOREIGN KEY ("B") REFERENCES "Task"("id") ON DELETE CASCADE ON UPDATE CASCADE;
