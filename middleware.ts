a7f3ef46a2ff1759dad50954a803db7a15c05263
1Omon <1serwise@gmail.com>
2025-02-26 04:57:21 +0000

    removing application page from public url

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,8 @@ import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import { decrypt } from "./lib/session";
 
-const protectedRoutes = ["/school/learning-spaace", "/school/admission-portal", "/administrator-office", "/Dashboard"]
-const publicRoutes = ["/", "/school/apply", "/guild", "/school", "/center"]
+const protectedRoutes = ["/school/learning-spaace", "/school/admission-portal", "/administrator-office", "/dashboard"]
+const publicRoutes = ["/", "/guild", "/school", "/center"]
 
 
 export default async function middleware(req: NextRequest){
