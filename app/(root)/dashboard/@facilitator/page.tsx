e66b9e5ede93be952a2a05e2cd91ab36a1d8879c
1Omon <1serwise@gmail.com>
2025-03-10 15:09:18 +0000

    add main and side section to facilitator dashboard

--- app/(root)/dashboard/@facilitator/page.tsx
+++ app/(root)/dashboard/@facilitator/page.tsx
@@ -1,8 +1,17 @@
+import MainContent from '@/app/school/(student)/learning-space/main-content'
+import SideSection from '@/app/school/(student)/learning-space/side-section'
 import React from 'react'
 
 const FacilitatorDashboard = () => {
   return (
-    <div>FacilitatorDashboard</div>
+    <div>
+        <MainContent>
+            Heya, I am the main
+        </MainContent>
+        <SideSection>
+            Hey, I am on the side
+        </SideSection>
+    </div>
   )
 }
 
